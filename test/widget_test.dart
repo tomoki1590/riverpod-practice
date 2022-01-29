4cf3466d6274cdf66d339f68c0739f8f872923e0
tomoki1590 <kaguratomato1@gmail.com>
2022-01-29 15:13:29 +0900

    flutterWidgetTest

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,37 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:riverpods_coutup/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(ProviderScope(child: const MyApp()));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('0'), findsNWidgets(3));
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Tap the '+' icbon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNWidgets(2));
+
+    await tester.tap(find.byIcon(CupertinoIcons.minus));
+    await tester.tap(find.byIcon(CupertinoIcons.minus));
+
+    await tester.pump();
+
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+    expect(find.text('-1'), findsOneWidget);
+    expect(find.text('2'), findsOneWidget);
   });
 }
